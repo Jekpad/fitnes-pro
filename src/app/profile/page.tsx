f4d83ff7691266b130f265630aae291de6f8150a
Daria <daria.aleks1@yandex.ru>
2024-08-24 11:44:10 +0300

    add

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,19 +1,20 @@
 function Profile() {
   return (
     <>
-      <div className="flex px-100 flex-col bg-#3f1919 w-1440px">
+      <div className="flex px-100 flex-col bg-[#fafafa;] w-[1440px] h-[1559px]">
+      <div className="top-[145px]">ШАПКА</div>
         <div className="flex items-start">
-          <p className="text-2xl text-start">Профиль</p>
+          <p className="text-[40px] text-start">Профиль</p>
         </div>
-        <div className="flex p-10 shadow-lg w-5/6">
+        <div className="flex p-10 shadow-lg w-5/6 bg-[white] rounded-2xl">
           <img src="" alt="Картинка" width="197px" height="100px" />
           <div>
-            <div>Даша</div>
-            <p>Логин: Даша</p>
-            <p>Пароль: лалала</p>
+            <div className="text-[32px]">Даша</div>
+            <p className="text-[18px]">Логин: Даша</p>
+            <p className="text-[18px]">Пароль: лалала</p>
             <button
               type="button"
-              className="text-gray-900 border bg-green-100 border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+              className="text-black border bg-[#bcec30] border-none focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full px-5 py-2.5 me-2 mb-2 dark:hover:border-gray-600 dark:focus:ring-gray-700 text-[18px]">
               Изменить пароль
             </button>
             <button
